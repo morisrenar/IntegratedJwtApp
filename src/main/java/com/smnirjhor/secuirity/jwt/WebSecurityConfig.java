ec5cefe7d7856f7fadd8c2df05a2486c9e0958d5
morisrenar <morisrenar@gmail.com>
2017-08-08 23:35:27 +0600

    single role of the auth is completed

---
+++ src/main/java/com/smnirjhor/secuirity/jwt/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.smnirjhor.secuirity.jwt;
+
+import com.smnirjhor.secuirity.jwt.config.AppAuthenticationEntryPoint;
+import com.smnirjhor.secuirity.jwt.config.MyAppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String USER = "USER";
+    public static final String ADMIN = "ADMIN";
+
+    @Autowired
+    private MyAppUserDetailsService myAppUserDetailsService;
+    @Autowired
+    private AppAuthenticationEntryPoint appAuthenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable().authorizeRequests()
+
+                .antMatchers("/guest/**").permitAll()
+                .antMatchers("/users/**").permitAll()
+                //.antMatchers("/user/**").hasRole(USER)
+                //.antMatchers("/admin/**").hasRole(ADMIN)
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Create a default account
+        /*auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");*/
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        auth.userDetailsService(myAppUserDetailsService);//.passwordEncoder(passwordEncoder);
+
+    }
+}
